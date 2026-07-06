f204d76cf18a1fa2e92fb53d2c5fe06e263e742a
Michael <michaelvoov@yandex.ru>
2026-07-06 22:21:56 +0500

    update

--- EquipmentLibraryV2-Avalonia/Views/MainWindow.axaml.cs
+++ EquipmentLibraryV2-Avalonia/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace EquipmentLibraryV2_Avalonia.Views
 {
@@ -7,6 +10,31 @@ namespace EquipmentLibraryV2_Avalonia.Views
         public MainWindow()
         {
             InitializeComponent();
+
+            if (OperatingSystem.IsWindows())
+            {
+                WindowDecorations = WindowDecorations.None;
+            }
+        }
+
+        private void Minimize_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestore_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+
+            MaximizeIcon.IsVisible = WindowState != WindowState.Maximized;
+            RestoreIcon.IsVisible = WindowState == WindowState.Maximized;
+        }
+
+        private void Close_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
