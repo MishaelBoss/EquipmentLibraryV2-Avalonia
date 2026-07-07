4bb467f215f4964969f4f5470eac3f5d3593beee
Michael <michaelvoov@yandex.ru>
2026-07-07 18:21:11 +0500

    Update

---
+++ EquipmentLibraryV2-Avalonia/Views/Components/ConnectionErrorUserControlView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace EquipmentLibraryV2_Avalonia.Views.Components;
+
+public partial class ConnectionErrorUserControlView : UserControl
+{
+    public ConnectionErrorUserControlView()
+    {
+        InitializeComponent();
+    }
+}
